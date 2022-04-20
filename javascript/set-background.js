605f8e0faaf8bff657c8084ff73f9a0488f3c47a
nathaneltitane <nathanel.titane@gmail.com>
2022-04-20 18:57:05 -0400

    Code & Features Update

---
+++ javascript/set-background.js
@@ -0,0 +1,23 @@
+// set background //
+
+$(document).ready(function() {
+
+	var count = 133;
+
+	function pad(str, max) {
+
+		str = str.toString();
+
+		return str.length < max ? pad("0" + str, max) : str;
+
+	}
+
+	// randomize images
+
+	$('.background').css(
+
+		'background-image',
+
+		'url("/background/background-' + pad(Math.floor(Math.random() * count), 3) + '.png ")'
+	);
+});
