beee0c3ecd311bc3be150c0b6c2d38eb31439bac
nathaneltitane <nathanel.titane@gmail.com>
2022-04-23 02:58:12 -0400

    Models update

--- javascript/modules.js
+++ javascript/modules.js
@@ -14,12 +14,6 @@ $(function() {
 
 	});
 
-	$.get("/modules/copyright.html", function(data) {
-
-		$("#copyright").append(data);
-
-	});
-
 	$.get("/modules/navigation.html", function(data) {
 
 		$("#navigation").append(data);
