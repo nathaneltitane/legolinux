7b8d0b455eceafdb766901d2148fe398fc656258
nathaneltitane <nathanel.titane@gmail.com>
2022-04-27 20:50:20 -0400

    Models update

--- javascript/drawer.js
+++ javascript/drawer.js
@@ -1,36 +1,39 @@
 // drawer //
 
-$('.drawer-button').click(function(e) {
+$(document).ready(function() {
 
-	// disable defaults prevention for href handling
+	$('.drawer-button').click(function(e) {
 
-	// e.preventDefault();
+		// disable defaults prevention for href handling
 
-	$('#drawer').toggleClass('drawer-open');
+		// e.preventDefault();
+
+		$('#drawer').toggleClass('drawer-open');
+
+	});
+
+	// prevent canvas interaction conflict
+
+		// prevent canvas interaction conflict
+
+	$('#background').click(function(e) {
+
+		// disable defaults prevention for href handling
+
+		// e.preventDefault();
+
+		$('#drawer').removeClass('drawer-open');
+
+	});
+
+	$('#model-canvas').click(function(e) {
+
+		// disable defaults prevention for href handling
+
+		// e.preventDefault();
+
+		$('#drawer').removeClass('drawer-open');
+
+	});
 
 });
-
-// prevent canvas interaction conflict
-
-// prevent canvas interaction conflict
-
-$('#background').click(function(e) {
-
-	// disable defaults prevention for href handling
-
-	// e.preventDefault();
-
-	$('#drawer').removeClass('drawer-open');
-
-});
-
-$('#model-canvas').click(function(e) {
-
-	// disable defaults prevention for href handling
-
-	// e.preventDefault();
-
-	$('#drawer').removeClass('drawer-open');
-
-});
-
