4beb535b49734f939e2f311d92700bd18f67649f
nathaneltitane <nathanel.titane@gmail.com>
2023-02-08 13:50:27 -0500

    1035195{message}

--- javascript/colors.js
+++ javascript/colors.js
@@ -1,13 +1,39 @@
-// color //
+// colors //
 
-$(document).ready(function() {
+$(function() {
+	// predefined color tiles //
 
-	$(".color").click(function() {
+	$(".color").click(function(){
 
 		var color = $(this).attr("data-value");
 
-		$("#canvas").css("background", color);
+		$("body").css("background-color", color);
 
 	});
 
+	// color selector //
+
+	var selection = document.getElementById('color-selector');
+
+	selection.addEventListener('change', function(e) {
+
+		var color = selection.value;
+
+		$("body").css("background-color", color);
+
+		$(".selector").css("background-color", color);
+
+		$(".selector").css("border-color", "#aaaaaaff");
+
+	},
+
+	false
+
+	);
 });
+
+function color_selector() {
+
+	$("#color-selector").click();
+
+}
