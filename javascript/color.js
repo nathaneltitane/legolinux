1ce955cd0483175b987dfcb5b5e2c50d0d50759f
nathaneltitane <nathanel.titane@gmail.com>
2023-02-07 23:22:56 -0500

    Code & Features Update

---
+++ javascript/color.js
@@ -0,0 +1,38 @@
+// color //
+
+$(function() {
+
+	$(".color").click(function() {
+
+		var color = $(this).attr("data-value");
+
+		$("#canvas").css("background-color", color);
+
+	});
+
+	if (Modernizr.inputtypes.color) {
+
+		$(".picker").css("display", 'inline-block');
+
+		var c = document.getElementById('color');
+
+		c.addEventListener('change', function(e) {
+
+			var color = c.value;
+
+			$("#canvas").css("background-color", color);
+
+			$(".picker").css("background-color", color);
+
+
+		},
+
+		false);
+	}
+});
+
+function color_select() {
+
+	$("#color").click();
+
+}
