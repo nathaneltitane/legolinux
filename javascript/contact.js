b8f5f46d5dbaf9a77ad775bc42b7d0a2270c5406
nathaneltitane <nathanel.titane@gmail.com>
2023-03-25 18:21:29 -0400

    Code & Features Update

--- javascript/contact.js
+++ javascript/contact.js
@@ -21,7 +21,11 @@ $(document).ready(function() {
 
 				id = id.trim();
 
+				id = id.toUpperCase();
+
 				name = name.trim();
+
+				name = name.toUpperCase();
 
 				$(button).on(
 					"click",
