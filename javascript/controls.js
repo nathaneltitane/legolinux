3b69db155c11010619f72f7e47694785dde13f26
nathaneltitane <nathanel.titane@gmail.com>
2023-10-01 23:56:23 -0400

    Code & Features Update

---
+++ javascript/controls.js
@@ -0,0 +1,15 @@
+// controls //
+
+$('.controls').click (function (e) {
+
+    // disable defaults prevention for href handling
+
+    // e.preventDefault ();
+
+    $('.controls-popup').toggleClass ('controls-show');
+
+});
+
+
+
+
