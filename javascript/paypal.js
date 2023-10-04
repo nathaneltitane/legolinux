aa5f127ddc290fb9314387c8509e5f63f70adba2
nathaneltitane <nathanel.titane@gmail.com>
2023-10-04 00:44:44 -0400

    Code & Features Update

--- javascript/paypal.js
+++ javascript/paypal.js
@@ -2,33 +2,13 @@
 
 $(document).ready (function () {
 
-    $('.paypal').click (function (e) {
+    $('.paypal').hover (function (e) {
 
         // disable defaults prevention for href handling
 
         // e.preventDefault ();
 
         $('.paypal-popup').toggleClass ('paypal-show');
-
-    });
-
-    $('#canvas').click (function (e) {
-
-        // disable defaults prevention for href handling
-
-        // e.preventDefaul t();
-
-        $('.paypal-popup').removeClass ('paypal-show');
-
-    });
-
-    $('#footer').click (function (e) {
-
-        // disable defaults prevention for href handling
-
-        // e.preventDefaul t();
-
-        $('.paypal-popup').removeClass ('paypal-show');
 
     });
 
