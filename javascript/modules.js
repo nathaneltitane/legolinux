bdc03945a71b74e676c75bc5505223a6de6a11db
nathaneltitane <nathanel.titane@gmail.com>
2023-10-18 19:51:40 -0400

    Code & Features Update

--- javascript/modules.js
+++ javascript/modules.js
@@ -6,66 +6,66 @@ $( function ( ) {
 
 		$ ( 'head' ).append ( data );
 
-	});
+	} );
 
 	$.get ( '/modules/drawer.html', function ( data ) {
 
 		$( '#drawer' ).append ( data );
 
-	});
+	} );
 
 	$.get ( '/modules/navigation.html', function ( data ) {
 
 		$( '#navigation' ).append ( data );
 
-	});
+	} );
 
 	$.get ( '/modules/contact.html', function ( data ) {
 
 		$( '#contact' ).append ( data );
 
-	});
+	} );
 
 	$.get ( '/modules/color.html', function ( data ) {
 
 		$( '#color' ).append ( data );
 
-	});
+	} );
 
 	$.get ( '/modules/colors.html', function ( data ) {
 
 		$( '#colors' ).append ( data );
 
-	});
+	} );
 
 	$.get ( '/modules/controls.html', function ( data ) {
 
 		$( '#controls' ).append ( data );
 
-	});
+	} );
 
 	$.get ( '/modules/edges.html', function ( data ) {
 
 		$( '#edges' ).append ( data );
 
-	});
+	} );
 
 	$.get ( '/modules/wireframe.html', function ( data ) {
 
 		$( '#wireframe' ).append ( data );
 
-	});
+	} );
 
 	$.get ( '/modules/helper.html', function ( data ) {
 
 		$( '#helper' ).append ( data );
 
-	});
+	} );
 
 	$.get ( '/modules/footer.html', function ( data ) {
 
 		$( '#footer' ).append ( data );
 
-	});
+	} );
 
-});
+} );
