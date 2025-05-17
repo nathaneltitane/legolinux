402182bb9b057276bf0ad2b4d72d3f001583839f
nathaneltitane <nathanel.titane@gmail.com>
2025-05-17 15:02:03 -0400

    Code & Features Update

--- javascript/samples.js
+++ javascript/samples.js
@@ -11,7 +11,7 @@ $( document ).ready ( function ( ) {
         '#edges-button-label',
         '#wireframe-button-label',
         '#controls-button-label',
-        '#minifig  -button-label'
+        '#minifig-button-label'
     ];
 
     var classes = [
