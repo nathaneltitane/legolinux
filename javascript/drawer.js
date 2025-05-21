c59429a761e93cf3268a43b0fb2fb2538f3590c0
nathaneltitane <nathanel.titane@gmail.com>
2025-05-21 19:31:53 -0400

    Code & Features Update

--- javascript/drawer.js
+++ javascript/drawer.js
@@ -89,7 +89,7 @@ $( document ).ready ( function ( ) {
 
     $( document ).on ( 'keyup', function ( event ) {
 
-        if ( event.key === 'k' ) {
+        if ( event.key === '#' ) {
 
             toggle ( );
 
