09e6335880643b4b16f1fc9bca9eed7aa370c861
nathaneltitane <nathanel.titane@gmail.com>
2025-12-01 20:25:30 -0500

    Code & Features Update

---
+++ javascript/selections.js
@@ -0,0 +1,81 @@
+// selections //
+
+$ ( document ) .ready ( function ( ) {
+
+	var canvas = document.getElementById ( 'canvas' ) ;
+
+	var selections = document.getElementById ( 'selections' ) ;
+
+	var isMouseOrTouchActive = false;
+
+	var resetTimeout;
+
+	function hide ( ) {
+
+		selections.style.marginTop = '-180px';
+
+	}
+
+	function show ( ) {
+
+		selections.style.marginBottom = '0px';
+
+	}
+
+	function showWithTimeout ( ) {
+
+		clearTimeout ( resetTimeout ) ;
+
+		resetTimeout = setTimeout ( function ( ) {
+
+			isMouseOrTouchActive = false;
+
+			show ( ) ;
+
+		}, 4000 ) ;
+
+	}
+
+	canvas.addEventListener ( 'mousemove', function ( event ) {
+
+		if ( !isMouseOrTouchActive ) {
+
+			isMouseOrTouchActive = true;
+
+			showWithTimeout ( ) ;
+
+			hide ( ) ;
+
+		}
+
+	} ) ;
+
+		canvas.addEventListener ( 'mouseenter', function ( event ) {
+
+		if ( !isMouseOrTouchActive ) {
+
+			isMouseOrTouchActive = true;
+
+			showWithTimeout ( ) ;
+
+			hide ( ) ;
+
+		}
+
+	} ) ;
+
+	canvas.addEventListener ( 'touchstart', function ( event ) {
+
+		if ( !isMouseOrTouchActive ) {
+
+			isMouseOrTouchActive = true;
+
+			showWithTimeout ( ) ;
+
+			hide ( ) ;
+
+		}
+
+	} ) ;
+
+} ) ;
