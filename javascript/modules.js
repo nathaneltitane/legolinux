8f8a73c57ab3198cfb493f1a453b2b3919bbf7eb
nathaneltitane <nathanel.titane@gmail.com>
2025-12-11 20:49:39 -0500

    Code & Features Update

--- javascript/modules.js
+++ javascript/modules.js
@@ -4,27 +4,23 @@ $( function ( ) {
 
 	// index //
 
-	if ( $( '#landing' ).length ) {
+	$.get ( '/modules/head.html', function ( data ) {
 
-		$.get ( '/modules/head.html', function ( data ) {
+		$( 'head' ).append ( data );
 
-			$( 'head' ).append ( data );
+	} );
 
-		} );
+	$.get ( '/modules/drawer.html', function ( data ) {
 
-		$.get ( '/modules/drawer.html', function ( data ) {
+		$( '#drawer' ).append ( data );
 
-			$( '#drawer' ).append ( data );
+	} );
 
-		} );
+	$.get ( '/modules/footer.html', function ( data ) {
 
-		$.get ( '/modules/footer.html', function ( data ) {
+		$( '#footer' ).append ( data );
 
-			$( '#footer' ).append ( data );
-
-		} );
-
-	}
+	} );
 
 	// standard //
 
