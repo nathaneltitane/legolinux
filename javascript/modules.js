74076cf8cb036ac645b1f742269ba3f0260261e8
nathaneltitane <nathanel.titane@gmail.com>
2025-12-23 22:11:27 -0500

    Code & Features Update

--- javascript/modules.js
+++ javascript/modules.js
@@ -8,9 +8,9 @@ $( function ( ) {
 
 		$.get ( '/modules/footer.html', function ( data ) {
 
-			$( '#footer' ).append ( data );
+			$( '#footer' ).append ( data ) ;
 
-		} );
+		} ) ;
 
 	}
 
@@ -20,11 +20,9 @@ $( function ( ) {
 
 		$.get ( '/modules/head.html', function ( data ) {
 
-			$( 'head' ).append ( data );
+			$( 'head' ).append ( data ) ;
 
-		} );
-
-
+		} ) ;
 
 	}
 
@@ -34,53 +32,53 @@ $( function ( ) {
 
 		$.get ( '/modules/home.html', function ( data ) {
 
-			$( '#home' ).append ( data );
+			$( '#home' ).append ( data ) ;
 
-		} );
+		} ) ;
 
 	}
 
 	$.get ( '/modules/drawer.html', function ( data ) {
 
-		$( '#drawer' ).append ( data );
+		$( '#drawer' ).append ( data ) ;
 
-	} );
+	} ) ;
 
 	$.get ( '/modules/contact-modal.html', function ( data ) {
 
-		$( '#contact-modal' ).append ( data );
+		$( '#contact-modal' ).append ( data ) ;
 
-	} );
+	} ) ;
 
 	$.get ( '/modules/contact.html', function ( data ) {
 
-		$( '#contact' ).append ( data );
+		$( '#contact' ).append ( data ) ;
 
-	} );
+	} ) ;
 
 	$.get ( '/modules/shop.html', function ( data ) {
 
-		$( '#shop' ).append ( data );
+		$( '#shop' ).append ( data ) ;
 
-	} );
+	} ) ;
 
 	$.get ( '/modules/github.html', function ( data ) {
 
-		$( '#github' ).append ( data );
+		$( '#github' ).append ( data ) ;
 
-	} );
+	} ) ;
 
 	$.get ( '/modules/donations.html', function ( data ) {
 
-		$( '#donations' ).append ( data );
+		$( '#donations' ).append ( data ) ;
 
-	} );
+	} ) ;
 
 	$.get ( '/modules/browse.html', function ( data ) {
 
-		$( '#browse' ).append ( data );
+		$( '#browse' ).append ( data ) ;
 
-	} );
+	} ) ;
 
 	// canvas controls //
 
@@ -88,70 +86,70 @@ $( function ( ) {
 
 		$.get ( '/modules/paypal.html', function ( data ) {
 
-			$( '#paypal' ).append ( data );
+			$( '#paypal' ).append ( data ) ;
 
-		} );
+		} ) ;
 
 		$.get ( '/modules/color.html', function ( data ) {
 
-			$( '#color' ).append ( data );
+			$( '#color' ).append ( data ) ;
 
-		} );
+		} ) ;
 
 		$.get ( '/modules/colors.html', function ( data ) {
 
-			$( '#colors' ).append ( data );
+			$( '#colors' ).append ( data ) ;
 
-		} );
+		} ) ;
 
 		$.get ( '/modules/dimensions.html', function ( data ) {
 
-			$( '#dimensions' ).append ( data );
+			$( '#dimensions' ).append ( data ) ;
 
-		} );
+		} ) ;
 
 		$.get ( '/modules/edges.html', function ( data ) {
 
-			$( '#edges' ).append ( data );
+			$( '#edges' ).append ( data ) ;
 
-		} );
+		} ) ;
 
 		$.get ( '/modules/camera.html', function ( data ) {
 
-			$( '#camera' ).append ( data );
+			$( '#camera' ).append ( data ) ;
 
-		} );
+		} ) ;
 
 		$.get ( '/modules/minifig.html', function ( data ) {
 
-			$( '#minifig' ).append ( data );
+			$( '#minifig' ).append ( data ) ;
 
-		} );
+		} ) ;
 
 		$.get ( '/modules/wireframe.html', function ( data ) {
 
-			$( '#wireframe' ).append ( data );
+			$( '#wireframe' ).append ( data ) ;
 
-		} );
+		} ) ;
 
 		$.get ( '/modules/controls.html', function ( data ) {
 
-			$( '#controls' ).append ( data );
+			$( '#controls' ).append ( data ) ;
 
-		} );
+		} ) ;
 
 		$.get ( '/modules/plane.html', function ( data ) {
 
-			$( '#plane' ).append ( data );
+			$( '#plane' ).append ( data ) ;
 
-		} );
+		} ) ;
 
 		$.get ( '/modules/helper.html', function ( data ) {
 
-			$( '#helper' ).append ( data );
+			$( '#helper' ).append ( data ) ;
 
-		} );
+		} ) ;
 
 	}
 
-} );
+} ) ;
